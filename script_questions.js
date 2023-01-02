e6e29dc0725e5837b6fbc015216d81b352699b88
ne-lap <nelapetrovic@hotmail.com>
2023-01-02 23:27:19 +0100

    questions load

--- script_questions.js
+++ script_questions.js
@@ -2,51 +2,51 @@ let questions = [];
 
 questions.push(
     {
-        qn: 'In which Italian city can you find the Colosseum?',
-        ans: ['Venice', 'Rome', 'Naples', 'Milan'],
-        correct_ans: 1
-    },
-    
-    {
-        qn: 'In the TV show New Girl, which actress plays Jessica Day?',
-        ans: ['Zooey Deschanel', 'Kaley Cuoco', 'Jennifer Aniston', 'Alyson Hannigan'],
-        correct_ans: 0
-    },
-    
-    {
-        qn: 'What is the largest canyon in the world?',
-        ans: ['Verdon Gorge, France', 'King`s Canyon, Australia', 'Grand Canyon, USA', 'Fjaðrárgljúfur Canyon, Iceland'],
+        qn: 'How many Infinity Stones are there?',
+        ans: ['3', '5', '6', '10'],
         correct_ans: 2
     },
     
     {
-        qn: 'How long is the border between the United States and Canada?',
-        ans: ['3,525 miles', '4,525 miles', '5,525 miles', '6,525 miles'],
+        qn: 'What is the only food that cannot go bad?',
+        ans: ['Dark chocolate', 'Peanut butter', 'Canned tuna', 'Honey'],
+        correct_ans: 3
+    },
+    
+    {
+        qn: 'Which was René Magritte`s first surrealist painting?',
+        ans: ['Not to Be Reproduced', 'Personal Values', 'The Lovers', 'The Lost Jockey'],
+        correct_ans: 3
+    },
+    
+    {
+        qn: 'What 90s boy band member bought Myspace in 2011?',
+        ans: ['Nick Lachey', 'Justin Timberlake', 'Shawn Stockman', 'AJ McLean'],
         correct_ans: 1
     },
     
     {
-        qn: 'What is the largest active volcano in the world?',
-        ans: ['Mount Etna', 'Mount Vesuvius', 'Mouna Loa', 'Mount Batur'],
+        qn: 'What is the most visited tourist attraction in the world?',
+        ans: ['Eiffel Tower', 'Statue of Liberty', 'Great Wall of China', 'Colosseum'],
         correct_ans: 0
     },
     
     {
-        qn: 'When did Lenin die?',
-        ans: ['21 January 1924', '21 January 1934', '21 January 1944', '21 January 1954'],
-        correct_ans: 0
+        qn: 'What`s the name of Hagrid`s pet spider?',
+        ans: ['Nigini', 'Crookshanks', 'Aragog', 'Mosag'],
+        correct_ans: 2
     },
     
     {
-        qn: 'In which US state did the Scopes Monkey Trial happen?',
-        ans: ['Maryland', 'South Dakota', 'Indiana', 'Tennessee'],
+        qn: 'What`s the heaviest organ in the human body?',
+        ans: ['Brain', 'Liver', 'Skin', 'Heart'],
         correct_ans: 1
     },
     
     {
-        qn: 'When was the book “To Kill a Mockingbird” by Harper Lee published?',
-        ans: ['1950', '1960', '1970', '1980'],
-        correct_ans: 1
+        qn: 'Who made the third most 3-pointers in the Playoffs in NBA history?',
+        ans: ['Kevin Durant', 'JJ Reddick', 'Lebron James', 'Kyle Korver'],
+        correct_ans: 2
     },
     
     {
@@ -56,8 +56,404 @@ questions.push(
     },
     
     {
-        qn: 'In which city can you find the Prado Museum?',
-        ans: ['Buenos Aires', 'Barcelona', 'Santiago','Madrid'],
+        qn: 'Which of these EU countries does not use the euro as its currency?',
+        ans: ['Poland', 'Denmark', 'Sweden','All of the above'],
         correct_ans: 3
+    },
+
+    {
+        qn: 'Which US city is the sunniest major city and sees more than 320 sunny days each year?',
+        ans: ['Phoenix', 'Miami', 'San Francisco', 'Austin'],
+        correct_ans: 0
+    },
+    
+    {
+        qn: 'What type of food holds the world record for being the most stolen around the globe?',
+        ans: ['Wagyu beef', 'Cheese ', 'Coffee', 'Chocolate'],
+        correct_ans: 1
+    },
+
+    {
+        qn: 'What element does the chemical symbol Au stand for?',
+        ans: ['Silver', 'Magnesium', 'Salt', 'Gold'],
+        correct_ans: 4
+    },
+    
+    {
+        qn: 'What is the highest-grossing Broadway show of all time?',
+        ans: ['The Lion King', 'Wicked', 'Kinky Boots', 'Hamilton'],
+        correct_ans: 0
+    },
+
+    {
+        qn: 'On average, how many seeds are located on the outside of a strawberry?',
+        ans: ['100', '200', '300', '400'],
+        correct_ans: 1
+    },
+    
+    {
+        qn: 'Which fast food restaurant has the largest number of retail locations in the world?',
+        ans: ['Jack In The Box', 'Chipotle', 'Subway', 'McDonald`s'],
+        correct_ans: 2
+    },
+
+    {
+        qn: 'Where is recognized as the location of the hottest temperature ever recorded on Earth?',
+        ans: ['Mitribah, Kuwait', 'Death Valley, California', 'Yuma, Arizona', 'Key West, Florida'],
+        correct_ans: 1
+    },
+
+    {
+        qn: 'What is the oldest soft drink in the United States?',
+        ans: ['Coca Cola', 'Pepsi', 'Dr. Pepper', 'Canada Dry Ginger Ale'],
+        correct_ans: 2
+    },
+    
+    {
+        qn: 'What river passes through New Orleans, Louisiana?',
+        ans: ['Orleans River', 'Mississippi River', 'Atchafalaya River', 'Colorado River'],
+        correct_ans: 1
+    },
+    
+    {
+        qn: 'In what country do more than half of people believe in elves?',
+        ans: ['Norway', 'Russia', 'Holland', 'Iceland'],
+        correct_ans: 3
+    },
+    
+    {
+        qn: 'What is the name of the coffee shop in the sitcom "Friends"?',
+        ans: ['Central Perk', 'Java Park', 'Central Park Coffee', 'Central Park Roastery'],
+        correct_ans: 0
+    },
+    
+    {
+        qn: 'Which pop star burnt down her home gym with candles?',
+        ans: ['Kim Kardashian', 'Lady Gaga', 'Zendaya', 'Britney Spears'],
+        correct_ans: 3
+    },
+    
+    {
+        qn: 'What is the highest-grossing video game franchise to date?',
+        ans: ['Mario', 'Pokemon', 'Call of Duty', 'Street Fighter'],
+        correct_ans: 1
+    },
+
+    {
+        qn: '"Cirque du Soleil" started in what country?',
+        ans: ['France', 'USA', 'Canada', 'Russia'],
+        correct_ans: 2
+    },
+    
+    {
+        qn: 'Which country`s national animal is a unicorn?',
+        ans: ['Scotland', 'Denmark', 'New Zealand', 'France'],
+        correct_ans: 0
+    },
+    
+    {
+        qn: 'What are the two highest-grossing films of all time?',
+        ans: ['Avengers: End Game and Star Wars: Episode VII: The Force Awakens', 'Avatar and Avengers: End Game', 'Avengers: Infinity War and Titanic', 'Furious 7 and Avatar'],
+        correct_ans: 1
+    },
+    
+    {
+        qn: 'Which Avenger other than Captain America was able to pick up Thor`s Mjolnir in the Marvel movies?',
+        ans: ['Wanda', 'Spiderman', 'Doctor Strange', 'Vision'],
+        correct_ans: 3
+    },
+    
+    {
+        qn: 'What is the main ingredient in a falafel?',
+        ans: ['Lentil', 'Chickpea', 'Broccoli', 'Split pea'],
+        correct_ans: 1
+    },
+    
+    {
+        qn: 'What color dresses do Chinese women traditionally wear on their wedding day?',
+        ans: ['Blue', 'Gold', 'White', 'Red'],
+        correct_ans: 3
+    },
+    
+    {
+        qn: 'What is yellowtail fish called in Japanese?',
+        ans: ['Ahi', 'Ikura', 'Hamachi', 'Maguro'],
+        correct_ans: 2
+    },
+
+    {
+        qn: 'What sport has been played on the moon?',
+        ans: ['Frisbee', 'Soccer', 'Golf', 'Bocce ball'],
+        correct_ans: 2
+    },
+    
+    {
+        qn: 'What is the highest-grossing holiday movie of all time?',
+        ans: ['Elf', 'Die Hard', 'It`s A Wonderful Life', 'Home Alone'],
+        correct_ans: 3
+    },
+    
+    {
+        qn: 'What was the first cash crop in America?',
+        ans: ['Tobacco', 'Corn', 'Sugar Cane', 'Grapes'],
+        correct_ans: 0
+    },
+
+    {
+        qn: 'Mycology is the scientific study of what?',
+        ans: ['Cancer cells', 'Flowers', 'Fungi', 'Blood'],
+        correct_ans: 2
+    },
+    
+    {
+        qn: 'What animal is the closest living relative of a human?',
+        ans: ['Gorillas', 'Monkeys', 'Bonobos', 'Homo sapiens'],
+        correct_ans: 2
+    },
+    
+    {
+        qn: 'What does the meaning of the word “zodiac” in Ancient Greek?',
+        ans: ['Circle of personalities', 'Circle of stars', 'Circle of animals', 'Circle of futures'],
+        correct_ans: 2
+    },
+    
+    {
+        qn: 'Aries, Sagittarius, and Leo are three zodiac signs in which triplicity?',
+        ans: ['Air', 'Water', 'Fire', 'Earth'],
+        correct_ans: 2
+    },
+    
+    {
+        qn: 'The first vaccine was for which disease?',
+        ans: ['Chickenpox', 'Polio', 'Measles', 'Smallpox'],
+        correct_ans: 3
+    },
+    
+    {
+        qn: 'Which Beatle made Elton John the godfather of his son?',
+        ans: ['Paul McCartney', 'John Lennon', 'Ringo Starr', 'George Harrison'],
+        correct_ans: 1
+    },
+    
+    {
+        qn: 'What lobster organ is made into a delicacy known as “tomalley”?',
+        ans: ['Claw', 'Tail', 'Liver', 'Antennae'],
+        correct_ans: 2
+    },
+    
+    {
+        qn: 'The villains in the 2007 version of the movie “Transformers” are known as what?',
+        ans: ['Decepticons', 'Storm Troopers', 'Death Eaters', 'Sentinels'],
+        correct_ans: 0
+    },
+    
+    {
+        qn: 'Which cereal grain is the most commonly used in beer',
+        ans: ['Wheat', 'Barley', 'Hops', 'Rice'],
+        correct_ans: 1
+    },
+    
+    {
+        qn: '"Indiana Jones and the Kingdom of the Crystal Skull" is based on whose story idea?',
+        ans: ['Michael Bay', 'Ernest Hemingway', 'Tim Burton', 'George Lucas'],
+        correct_ans: 3
+    },
+    
+    {
+        qn: 'What country are the Galapagos Islands located in?',
+        ans: ['Belize', 'Brazil', 'Ecuador', 'Colombia'],
+        correct_ans: 2
+    },
+    
+    {
+        qn: 'Which sea creature has three hearts?',
+        ans: ['Shark', 'Jellyfish', 'Stingray', 'Octopus'],
+        correct_ans: 3
+    },
+    
+    {
+        qn: 'Pupusas are from what country?',
+        ans: ['Mexico', 'El Salvador', 'Brazil', 'Poland'],
+        correct_ans: 1
+    },
+    
+    {
+        qn: 'What is the name of the talkative parrot in "Aladdin"?',
+        ans: ['Pascal', 'Meeko', 'Iago', 'Abu'],
+        correct_ans: 2
+    },
+    
+    {
+        qn: 'On "Friends", what is Ross` occupation?',
+        ans: ['Paleontologist', 'Meteorologist', 'News Anchor', 'Teacher'],
+        correct_ans: 0
+    },
+    
+    {
+        qn: 'What is Rick`s last name in "Rick and Morty"?',
+        ans: ['Sanchez', 'Smith', 'Guetterman', 'Noopers'],
+        correct_ans: 0
+    },
+    
+    {
+        qn: 'Where did the croissant originate?',
+        ans: ['France', 'Austria', 'Turkey', 'Tokyo'],
+        correct_ans: 1
+    },
+    
+    {
+        qn: 'In what U.S. state does "Breaking Bad" take place?',
+        ans: ['Utah', 'Missouri', 'Texas', 'New Mexico'],
+        correct_ans: 3
+    },
+    
+    {
+        qn: 'What country is Shakira from?',
+        ans: ['Mexico', 'Puerto Rico', 'Paraguay', 'Colombia'],
+        correct_ans: 3
+    },
+    
+    {
+        qn: 'Which of these retailers is the oldest operating?',
+        ans: ['Macy`s', 'Brooks Brothers', 'Kiehl`s', 'Saks Fifth Avenue'],
+        correct_ans: 1
+    },
+    
+    {
+        qn: 'Mount Olympus is the highest mountain in what country?',
+        ans: ['Italy', 'Spain', 'Portugal', 'Greece'],
+        correct_ans: 3
+    },
+    
+    {
+        qn: 'Which country ranks first in cereal consumption per capita?',
+        ans: ['USA', 'Italy', 'Ireland', 'Philippines'],
+        correct_ans: 2
+    },
+    
+    {
+        qn: 'From which country do French fries originate?',
+        ans: ['Belgium', 'France', 'USA', 'England'],
+        correct_ans: 0
+    },
+    
+    {
+        qn: 'What sport is referred to as the "sport of kings"?',
+        ans: ['Bocce Ball', 'Cricket', 'Rugby', 'Polo'],
+        correct_ans: 3
+    },
+    
+    {
+        qn: 'Who was the first Disney Princess?',
+        ans: ['Cinderella', 'Snow White', 'Aurora', 'Jasmine'],
+        correct_ans: 1
+    },
+    
+    {
+        qn: 'What year was In-N-Out Burger founded?',
+        ans: ['1968', '1975', '1948', '1985'],
+        correct_ans: 2
+    },
+    
+    {
+        qn: 'How many islands are in Hawaii?',
+        ans: ['4', '7', '48', '137'],
+        correct_ans: 3
+    },
+    
+    {
+        qn: 'What is the national alcoholic beverage of America?',
+        ans: ['Rye', 'Bourbon', 'Whiskey', 'Cognac'],
+        correct_ans: 1
+    },
+    
+    {
+        qn: 'What is Bruno Mars` real name?',
+        ans: ['Peter Gene Hernandez', 'Michael Nguyen-Stevenson', 'Ray Aguirre', 'Bruno Maritza'],
+        correct_ans: 0
+    },
+    
+    {
+        qn: 'What was the very first Pixar movie?',
+        ans: ['Wall-E', 'Monsters, Inc.', 'A Bug`s Life', 'Toy Story'],
+        correct_ans: 3
+    },
+    
+    {
+        qn: 'What was the ancient Goddess of Love?',
+        ans: ['Athena', 'Venus', 'Helen', 'Hera'],
+        correct_ans: 1
+    },
+    
+    {
+        qn: 'What are imitation crab sticks made of?',
+        ans: ['Whitefish', 'Cuttlefish', 'Tofu', 'Calamari'],
+        correct_ans: 0
+    },
+    
+    {
+        qn: 'Which ocean is the Bermuda Triangle in?',
+        ans: ['Pacific Ocean', 'Atlantic Ocean', 'Caribbean Ocean', 'Indian Ocean'],
+        correct_ans: 1
+    },
+    
+    {
+        qn: 'Where is Kanye West from?',
+        ans: ['Chicago, Illinois', 'Boston, Massachusetts', 'Detroit, Michigan', 'Los Angeles, California'],
+        correct_ans: 0
+    },
+    
+    {
+        qn: 'What is the smallest country in the world?',
+        ans: ['Luxembourg', 'The Vatican City', 'Palau', 'San Marino'],
+        correct_ans: 1
+    },
+    
+    {
+        qn: 'What animal species is the only male that gets pregnant?',
+        ans: ['Dolphin', 'Elephant Seal', 'Sea Turtle', 'Seahorse'],
+        correct_ans: 3
+    },
+
+    {
+        qn: 'Who was the first woman to ever be inducted into the Rock and Roll Hall of Fame?',
+        ans: ['Dolly Parton', 'Janis Joplin', 'Joni Mitchell', 'Aretha Franklin'],
+        correct_ans: 3
+    },
+
+    {
+        qn: 'What is the hardest natural substance in the world?',
+        ans: ['Granite', 'Marble', 'Iron', 'Diamond'],
+        correct_ans: 3
+    },
+
+    {
+        qn: 'Which "Wonders Of The World" is still in existence',
+        ans: ['Temple of Artemis', 'Pyramids of Giza', 'Hanging Gardens of Babylon', 'Statue of Zeus'],
+        correct_ans: 1
+    },
+
+    {
+        qn: 'Who painted the Mona Lisa?',
+        ans: ['Michelangelo', 'Vincent van Gogh', 'Rembrandt', 'Leonardo da Vinci'],
+        correct_ans: 3
+    },
+
+    {
+        qn: 'Which company was the first to reach 1 trillion dollars?',
+        ans: ['Microsoft', 'Apple', 'Amazon', 'Google'],
+        correct_ans: 1
+    },
+
+    {
+        qn: 'Which animal has the largest eyes?',
+        ans: ['Giant squid', 'Blue whale', 'Ostrich', 'Elephant'],
+        correct_ans: 0
+    },
+
+    {
+        qn: 'From what country did jazz originate?',
+        ans: ['France', 'USA', 'Canada', 'Germany'],
+        correct_ans: 1
     }
 );
